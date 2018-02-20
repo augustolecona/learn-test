c4e65ca275a7a7774b5cb9c8d6db0f3259107477
Isaac Kamga <u2isaac@gmail.com>
2018-02-20 21:17:59 +0100

    Ensure that brewing Espresso consumes beans

--- src/test/java/io/learning/testing/CafeTest.java
+++ src/test/java/io/learning/testing/CafeTest.java
@@ -8,15 +8,30 @@ public class CafeTest {
     @Test
     public void canBrewEspresso()
     {
+        // Given
         Cafe cafe = new Cafe();
         cafe.restockBeans(8);
 
+        // When
         final Coffee coffee = cafe.brew(CoffeeType.Espresso);
 
-
+        // Then
         // Ensure that it's an espresso, it has no milk and it has enough coffee
         Assert.assertEquals(CoffeeType.Espresso, coffee.getType());
         Assert.assertEquals(0, coffee.getMilk());
         Assert.assertEquals(8, coffee.getBeans());
+    }
+
+    public void brewEspressoConsumesBeans()
+    {
+        // Given
+        Cafe cafe = new Cafe();
+        cafe.restockBeans(8);
+
+        // When
+        Coffee coffee = cafe.brew(CoffeeType.Espresso);
+
+        // Then
+        Assert.assertEquals(0, cafe.getBeansInStock());
     }
 }
