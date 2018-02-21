1827504ac38479b695fa9f361e79ae567aee615a
Isaac Kamga <u2isaac@gmail.com>
2018-02-21 12:31:47 +0100

    Test that a leap year is divisible by 4 and 400 but not divisible by 100

--- src/test/java/io/learning/testing/LeapYearTest.java
+++ src/test/java/io/learning/testing/LeapYearTest.java
@@ -1,14 +1,34 @@
 package io.learning.testing;
 
 import org.junit.Assert;
+import org.junit.Test;
 
 public class LeapYearTest {
 
     //A leap year is one which is divisible by both 4 and 400 but isn't divisible by 100
 
-    public void leapYearsShouldBeDivisibleByFour()
+    @Test
+    public void leapYearIsBeDivisibleByFour()
     {
-        Assert.assertTrue(LeapYear.isLeap(2016));
+        Assert.assertTrue(LeapYear.isLeapYear(2016));
+    }
+
+    @Test
+    public void normalYearIsNotDivisibleByFour()
+    {
+        Assert.assertFalse(LeapYear.isLeapYear(2017));
+    }
+
+    @Test
+    public void leapYearIsNotDivisibleByOneHundred()
+    {
+        Assert.assertFalse(LeapYear.isLeapYear(1900));
+    }
+
+    @Test
+    public void leapYearIsDivisibleByFourHundred()
+    {
+        Assert.assertTrue(LeapYear.isLeapYear(2000));
     }
 
 }
