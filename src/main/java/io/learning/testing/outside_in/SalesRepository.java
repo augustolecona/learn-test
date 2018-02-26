e7cfe16243223b03f2f0eb6af76e1a7bfb9aa2fe
Isaac Kamga <u2isaac@gmail.com>
2018-02-26 16:50:00 +0100

    Start from Sales Repository

---
+++ src/main/java/io/learning/testing/outside_in/SalesRepository.java
@@ -0,0 +1,12 @@
+package io.learning.testing.outside_in;
+
+public class SalesRepository {
+
+    private final String fileLocation;
+
+    public SalesRepository(String fileLocation) {
+
+        this.fileLocation = fileLocation;
+
+    }
+}
