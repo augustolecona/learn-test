9e80c557680e1c4228f5758dde515ad09aa3dc59
Isaac Kamga <u2isaac@gmail.com>
2018-02-26 16:51:16 +0100

    Report from Sales Analyser @Service

---
+++ src/main/java/io/learning/testing/outside_in/SalesReport.java
@@ -0,0 +1,15 @@
+package io.learning.testing.outside_in;
+
+public class SalesReport {
+
+    private final SalesAnalyser salesAnalyser;
+
+    public SalesReport(SalesAnalyser salesAnalyser) {
+        this.salesAnalyser = salesAnalyser;
+    }
+
+    public void run(){
+
+        // TODO Print things out here
+    }
+}
