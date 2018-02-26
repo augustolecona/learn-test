d60a65cc04b0f72207aace846ea867e0c9e29bd3
Isaac Kamga <u2isaac@gmail.com>
2018-02-26 17:22:01 +0100

    Sales Analyser @Service

--- src/main/java/io/learning/testing/outside_in/SalesAnalyser.java
+++ src/main/java/io/learning/testing/outside_in/SalesAnalyser.java
@@ -1,10 +1,16 @@
 package io.learning.testing.outside_in;
 
+import java.util.Map;
+
 public class SalesAnalyser {
 
     private final SalesRepository salesRepository;
 
     public SalesAnalyser(SalesRepository salesRepository) {
         this.salesRepository = salesRepository;
+    }
+
+    public Map<String, Integer> tallyCitySales(){
+        return null;
     }
 }
