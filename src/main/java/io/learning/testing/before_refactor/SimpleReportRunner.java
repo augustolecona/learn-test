d078f903533d8f28b5b5a29744774a34b353660a
Isaac Kamga <u2isaac@gmail.com>
2018-02-26 14:22:39 +0100

    Sales Report runner

---
+++ src/main/java/io/learning/testing/before_refactor/SimpleReportRunner.java
@@ -0,0 +1,18 @@
+package io.learning.testing.before_refactor;
+
+public class SimpleReportRunner {
+
+    public static void main(String[] args)
+    {
+        if (args.length < 1)
+        {
+            System.err.println("You must provide a commandline argument specifying the file to analyse");
+            System.exit(-1);
+        }
+
+        CsvSalesRepository repo = new CsvSalesRepository(args[0]);
+        final SalesAnalysisService analyser = new SalesAnalysisService(repo);
+        SalesReport report = new SalesReport(System.out, analyser);
+        report.report();
+    }
+}
